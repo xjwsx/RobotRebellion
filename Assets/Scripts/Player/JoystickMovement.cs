c8dc3e3835b6cde8c24bb93a9aec306076ebe970
xjwsx <134588087+xjwsx@users.noreply.github.com>
2024-03-19 01:35:46 +0900

    joystickMovement
    조이스틱 움직임 구현

---
+++ Assets/Scripts/Player/JoystickMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickMovement : MonoBehaviour
+{
+    public static JoystickMovement instance;
+    public GameObject bigStick;
+    public GameObject smallStick;
+    Vector3 stickFirstPosition;
+    public Vector3 joyVec;
+    Vector3 joystickFirstPosition;
+    float stickRadius;
+
+    public bool isPlayerMoving = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    private void Start()
+    {
+        stickRadius = bigStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+        joystickFirstPosition = bigStick.transform.position;
+    }
+
+    public void PointDown()
+    {
+        bigStick.transform.position = Input.mousePosition;
+        smallStick.transform.position = Input.mousePosition;
+        stickFirstPosition = Input.mousePosition;
+        PlayerController.instance.animator.SetBool("Shot", false);
+        PlayerController.instance.animator.SetBool("Run", true);
+        isPlayerMoving = true;
+        PlayerTargeting.instance.getATarget = false;
+    }
+
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3 DragPosition = pointerEventData.position;
+        joyVec = (DragPosition - stickFirstPosition).normalized;
+        float stickDistance = Vector3.Distance(DragPosition, stickFirstPosition);
+
+        if(stickDistance < stickRadius)
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
+        }
+        else
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
+        }
+    }
+
+    public void Drop()
+    {
+        joyVec = Vector3.zero;
+        bigStick.transform.position = joystickFirstPosition;
+        smallStick.transform.position = joystickFirstPosition;
+        PlayerController.instance.animator.SetBool("Run", false);
+        isPlayerMoving = false;
+    }
+}
